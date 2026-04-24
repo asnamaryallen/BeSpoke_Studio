a9aaf2e8f4c0eabed5ac1e15d4a3f9090aa8afaa
asnamaryallen <your.github.email@example.com>
2026-04-24 10:43:42 +0530

    resolving

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aao#llm0$*pd3ibf8wk=670+#7wfqz)ue$wtu$$=pg^@0-x_t5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
